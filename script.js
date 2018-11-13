2f3d1e6b60c89c3f913e277679c9405ecf22a4fc
tomual <tomual@users.noreply.github.com>
2018-11-13 21:05:51 +0800

    Sprites to use texture atlas

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ let TextureCache = PIXI.utils.TextureCache
 
 PIXI.utils.sayHello();
 
-let app = new Application({ width: 800, height: 450 });
+let app = new Application({ width: 800, height: 450, antialias: true });
 document.body.appendChild(app.view);
 
 let dungeon;
@@ -15,11 +15,36 @@ let intelligence = 10;
 let attackSpeed = 1000;
 let lastAttacked = 0;
 
-loader.load(setup);
+let player =
+
+    loader
+    .add("img/bg.png")
+    .add("img/player.json")
+    .add("img/enemy.json")
+    .load(setup);
 
 // Initialise
 function setup() {
-	dungeon = new Sprite();
+    let bg = new Sprite(resources["img/bg.png"].texture);
+    app.stage.addChild(bg);
+
+    let playerTexture = resources["img/player.json"].textures;
+    let enemyTexture = resources["img/enemy.json"].textures;
+
+    let player = new Sprite(playerTexture["player-stand.png"]);
+    player.height = player.height / 3;
+    player.width = player.width / 3;
+    player.x = 230;
+    player.y = 250;
+    let enemy = new Sprite(enemyTexture["enemy-stand.png"]);
+    enemy.height = enemy.height / 3;
+    enemy.width = enemy.width / 3;
+    enemy.x = 590;
+    enemy.y = 250;
+    app.stage.addChild(player);
+    app.stage.addChild(enemy);
+
+    dungeon = new Sprite();
     app.stage.addChild(dungeon);
     drawEnemyHealth();
     state = world;
@@ -33,48 +58,48 @@ function tick(delta) {
 
 // World stage
 function world(delta) {
-	if (performance.now() - lastAttacked > attackSpeed) {
-		lastAttacked = performance.now();
-		attack();
-	}
+    if (performance.now() - lastAttacked > attackSpeed) {
+        lastAttacked = performance.now();
+        attack();
+    }
 }
 
 // Enemy health
 function drawEnemyHealth() {
-	enemyHealth = new PIXI.Container();
-	enemyHealth.position.set(app.screen.width - 200, 40);
-	app.stage.addChild(enemyHealth);
+    enemyHealth = new PIXI.Container();
+    enemyHealth.position.set(app.screen.width - 200, 40);
+    app.stage.addChild(enemyHealth);
 
-	let innerBar = new PIXI.Graphics();
-	innerBar.beginFill(0x000000);
-	innerBar.drawRect(0, 0, 128, 8);
-	innerBar.endFill();
-	enemyHealth.addChild(innerBar);
+    let innerBar = new PIXI.Graphics();
+    innerBar.beginFill(0x000000);
+    innerBar.drawRect(0, 0, 128, 8);
+    innerBar.endFill();
+    enemyHealth.addChild(innerBar);
 
-	let outerBar = new PIXI.Graphics();
-	outerBar.beginFill(0xFF3300);
-	outerBar.drawRect(0, 0, 128, 8);
-	outerBar.endFill();
-	enemyHealth.addChild(outerBar);
+    let outerBar = new PIXI.Graphics();
+    outerBar.beginFill(0xFF3300);
+    outerBar.drawRect(0, 0, 128, 8);
+    outerBar.endFill();
+    enemyHealth.addChild(outerBar);
 
-	enemyHealth.outer = outerBar;
+    enemyHealth.outer = outerBar;
 }
 
 // Attack enemy
 function attack() {
-	let damage = getDamage(intelligence);
-	let endHealth = enemyHealth.outer.width - damage;
-	if (endHealth <= 0) {
-		enemyHealth.outer.width = 0;
-		enemyDie();
-	} else {
-		enemyHealth.outer.width -= getDamage(intelligence);
-	}
+    let damage = getDamage(intelligence);
+    let endHealth = enemyHealth.outer.width - damage;
+    if (endHealth <= 0) {
+        enemyHealth.outer.width = 0;
+        enemyDie();
+    } else {
+        enemyHealth.outer.width -= getDamage(intelligence);
+    }
 }
 
 // Enemy death
 function enemyDie() {
-	console.log('enemy died');
+    console.log('enemy died');
 }
 
 // Calculate damage
